101d5aca0d95ba0c362ec8b4df5629523f307b71
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-08-14 11:06:47 +0200

    Add parameter part example

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -3,15 +3,13 @@
 defined('TYPO3') || die('Access denied.');
 
 call_user_func(function ($_EXTKEY = 'traw_theme', $table = 'tt_content') {
-    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItemGroup($table, 'CType', $_EXTKEY, 'TRAW-Theme');
-    $cTypes = [
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItemGroup($table, 'CType', $_EXTKEY, 'TRAW-Theme Test Elements');
+    \TRAW\VhsCol\Configuration\CTypes::registerCTypes([
         [
-            'config' => [
-                'label' => 'Extension loaded',
-                'value' => 'traw_extloaded',
-                'icon' => 'module-extensionmanager',
-                'group' => $_EXTKEY,
-            ],
+            'label' => 'Extension loaded',
+            'value' => 'traw_extloaded',
+            'icon' => 'module-extensionmanager',
+            'group' => $_EXTKEY,
             'showitem' => '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
                             --palette--;;general,
                             --palette--;;headers,
@@ -40,12 +38,10 @@ call_user_func(function ($_EXTKEY = 'traw_theme', $table = 'tt_content') {
             'flexform' => 'FILE:EXT:traw_theme/Configuration/Flexform/ExtensionLoaded.xml',
         ],
         [
-            'config' => [
-                'label' => 'Date Range',
-                'value' => 'traw_daterange',
-                'icon' => 'form-date-picker',
-                'group' => $_EXTKEY,
-            ],
+            'label' => 'Date Range',
+            'value' => 'traw_daterange',
+            'icon' => 'form-date-picker',
+            'group' => $_EXTKEY,
             'showitem' => '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
                             --palette--;;general,
                             --palette--;;headers,
@@ -72,22 +68,29 @@ call_user_func(function ($_EXTKEY = 'traw_theme', $table = 'tt_content') {
                 ],
             ],
         ],
-
-    ];
-
-    foreach ($cTypes as $cType) {
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTcaSelectItem($table, 'CType', $cType['config']);
-        $GLOBALS['TCA'][$table]['ctrl']['typeicon_classes'][$cType['config']['value']] = $cType['config']['icon'];
-        if (!empty($cType['showitem'])) {
-            $GLOBALS['TCA'][$table]['types'][$cType['config']['value']]['showitem'] = $cType['showitem'];
-        }
-        if (!empty($cType['columnsOverrides'])) {
-            $GLOBALS['TCA'][$table]['types'][$cType['config']['value']]['columnsOverrides'] = $cType['columnsOverrides'];
-        }
-        if (!empty($cType['flexform'])) {
-            \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPiFlexFormValue('', $cType['flexform'], $cType['config']['value']);
-        }
-    }
+        [
+            'label' => 'Parameter Part test',
+            'value'=>'traw_parampart',
+            'group' => $_EXTKEY,
+            'icon' => 'actions-link',
+            'showitem' => '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
+                --palette--;;general,
+                --palette--;;headers,
+                --div--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:tabs.appearance,
+                --palette--;;frames,
+                --palette--;;appearanceLinks,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:language,
+                --palette--;;language,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:access,
+                --palette--;;hidden,
+                --palette--;LLL:EXT:frontend/Resources/Private/Language/locallang_ttc.xlf:palette.access;access,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:categories,
+                --div--;LLL:EXT:core/Resources/Private/Language/locallang_tca.xlf:sys_category.tabs.category,categories,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,rowDescription,,
+                --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended
+                ',
+        ]
+    ]);
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, [
         'dateRangeStart' => [
@@ -105,7 +108,7 @@ call_user_func(function ($_EXTKEY = 'traw_theme', $table = 'tt_content') {
             ],
         ],
     ]);
-    $GLOBALS['TCA'][$table]['palettes']['dateRange'] =[
+    $GLOBALS['TCA'][$table]['palettes']['dateRange'] = [
         'label' => 'Date range',
         'showitem' => 'dateRangeStart, dateRangeEnd',
     ];
