1a0fd761ad6f115039975bb16c339fcb14c8e76a
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2024-10-02 13:04:24 +0200

    Test Preview-Renderer for itemsProcFunc items

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -70,7 +70,7 @@ call_user_func(function ($_EXTKEY = 'traw_theme', $table = 'tt_content') {
         ],
         [
             'label' => 'Parameter Part test',
-            'value'=>'traw_parampart',
+            'value' => 'traw_parampart',
             'group' => $_EXTKEY,
             'icon' => 'actions-link',
             'showitem' => '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
@@ -91,8 +91,9 @@ call_user_func(function ($_EXTKEY = 'traw_theme', $table = 'tt_content') {
                 ',
         ],
         [
+            'previewRenderer' => \TRAW\TrawTheme\Backend\PreviewRenderer::class,
             'label' => 'Pipe to BR test',
-            'value'=>'traw_pipe2br',
+            'value' => 'traw_pipe2br',
             'group' => $_EXTKEY,
             'icon' => 'actions-arrow-down-right',
             'showitem' => '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
@@ -115,7 +116,7 @@ call_user_func(function ($_EXTKEY = 'traw_theme', $table = 'tt_content') {
         ],
         [
             'label' => 'Phone Number',
-            'value'=>'traw_phone',
+            'value' => 'traw_phone',
             'group' => $_EXTKEY,
             'icon' => 'actions-phone',
             'showitem' => '--div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:general,
@@ -136,7 +137,7 @@ call_user_func(function ($_EXTKEY = 'traw_theme', $table = 'tt_content') {
                 --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:notes,rowDescription,,
                 --div--;LLL:EXT:core/Resources/Private/Language/Form/locallang_tabs.xlf:extended
                 ',
-        ]
+        ],
     ]);
 
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addTCAcolumns($table, [
@@ -154,10 +155,40 @@ call_user_func(function ($_EXTKEY = 'traw_theme', $table = 'tt_content') {
                 'format' => 'date',
             ],
         ],
+        'test_field' => [
+            'label' => 'My field',
+            'config' => [
+                'type' => 'select',
+                'renderType' => 'selectSingle',
+                'items' => [
+                    ['label' => 'Added with TCA', 'value' => 'tca-value'],
+                ],
+                'itemsProcFunc' => \TRAW\VhsCol\Configuration\TcaOptionsMap::class . '->addOptions',
+            ],
+        ],
     ]);
+
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addToAllTCAtypes($table, 'test_field', 'traw_pipe2br', 'after:header');
     $GLOBALS['TCA'][$table]['palettes']['dateRange'] = [
         'label' => 'Date range',
         'showitem' => 'dateRangeStart, dateRangeEnd',
+    ];
+
+
+    $GLOBALS['TCA'][$table]['tx_vhscol_option_map'] = [
+        'test_field' => [
+            [
+                'conditions' => [
+                    'fields' => [
+                        'CType' => ['traw_pipe2br'],
+                        'layout' => [0],
+                    ],
+                ],
+                'options' => [
+                    ['label' => 'Added with itemsProc label', 'value' => 'itemsProc-value'],
+                ],
+            ],
+        ],
     ];
 
 });
