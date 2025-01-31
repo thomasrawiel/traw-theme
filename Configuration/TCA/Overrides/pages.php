b3f60645acc2d59265fa423c612b7b2cda8dd46e
Thomas Rawiel <thomasrawiel@users.noreply.github.com>
2025-01-31 09:18:52 +0100

    Testing doktype registration

--- Configuration/TCA/Overrides/pages.php
+++ Configuration/TCA/Overrides/pages.php
@@ -13,4 +13,22 @@ call_user_func(function ($_EXTKEY = 'traw_theme', $table = 'pages') {
         'Configuration/TSConfig/Page.tsconfig',
         'TRAW Theme Page TSConfig'
     );
+
+    \TRAW\VhsCol\Configuration\Doktypes::registerDoktypes([
+        [
+            'label' => 'My Events',
+            'value' => 22,
+            'icon' => 'actions-calendar',
+            'group' => 'traw-doktype',
+        ],
+        [
+            'label' => 'My special Type',
+            'value' => 23,
+            'icon' => 'apps-pagetree-folder-contains-tt-address',
+            'group' => 'traw-doktype',
+            'itemType' => \TYPO3\CMS\Core\Domain\Repository\PageRepository::DOKTYPE_SYSFOLDER,
+            'allowedTables' => 'tt_content,tt_address',
+
+        ],
+    ], 'TRAW Theme Doktypes');
 });
